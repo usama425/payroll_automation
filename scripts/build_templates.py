64d03fe323f704062f54fc7cf754ebc3f5998c65
usama425 <usama.abdullah425@gmail.com>
2026-07-02 11:42:11 +0300

    hfkjdajawefdaew

--- scripts/build_templates.py
+++ scripts/build_templates.py
@@ -15,8 +15,7 @@ def cm(target, header=None, fallback=None, transform="trim_whitespace", required
         "is_required": required,
     }
 
-
-def f(target, header=None, fallback=None, required=0):
+    def f(target, header=None, fallback=None, required=0):
     """Float-valued column."""
     return cm(target, header, fallback, "to_float", required)
 
